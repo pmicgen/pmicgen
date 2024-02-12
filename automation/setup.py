bec64914a0b6946d772cdd9c08e476de2386f372
Mario1159 <mario@1159.cl>
2024-02-11 23:25:06 -0300

    Separate local installation from colab

--- automation/setup.py
+++ automation/setup.py
@@ -4,6 +4,9 @@ VERSION = "0.0.1"
 DESCRIPTION = "Automated generation of an PMIC for SKY130"
 LONG_DESCRIPTION = "Automated generation of an PMIC for SKY130"
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="pmicgen",
     version=VERSION,
@@ -12,5 +15,6 @@ setup(
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
+    install_requires=requirements,
     entry_points={"console_scripts": ["pmicgen = cli.main:main"]},
 )

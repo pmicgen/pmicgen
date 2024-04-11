8aeca0c6c1e4728e8c7cc2e06308b04889c16734
Mario Romero <mario@1159.cl>
2024-04-11 15:13:46 -0400

    Add form to get user input

---
+++ analysis/ldo.py
@@ -0,0 +1,26 @@
+import numpy as np
+import analysis.ldo_mna as mna
+import matplotlib.pyplot as plt
+import sympy as sym
+
+class LDO:
+    def __init__(self, psr_condition, load_regulation_condition, phase_margin_condition, size_condition):
+        if psr_condition=="min":
+            self.psr_condition = float('inf')
+        else:
+            self.psr_condition = psr_condition
+
+        if load_regulation_condition=="min":
+            self.load_regulation_condition = float('inf')
+        else:
+            self.load_regulation_condition = load_regulation_condition
+
+        if phase_margin_condition=="max":
+            self.phase_margin_condition = float('-inf')
+        else:
+            self.phase_margin_condition = phase_margin_condition
+
+        if size_condition=="min":
+            self.size_condition = float('inf')
+        else:
+            self.size_condition = size_condition

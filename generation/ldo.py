5e5496d57e863cfc6cafef39fe56b1e58f95c25a
Mario Romero <mario@1159.cl>
2024-04-14 21:43:23 -0400

    Add layout view changes

--- generation/ldo.py
+++ generation/ldo.py
@@ -1,4 +1,18 @@
 from .component import *
 
+import gdsfactory as gf
+import sky130
+
 class LDO(LDOComponent):
-    pass
+
+    def __init__(self, tech: TechManager):
+        super().__init__(tech)
+
+    def generate(self):
+        bgr = gf.read.import_gds("build/sky130_bgr/gds/bgr.gds")
+        erroramp = gf.read.import_gds("build/sky130_erroramp/gds/erroramp.gds").flatten_offgrid_references()
+        pmosw = gf.read.import_gds("build/sky130_pmosw/gds/pmosw.gds")
+
+        ldo = gf.pack([bgr, erroramp, pmosw])
+        ldo[0].flatten_offgrid_references().write_gds("build/sky130_ldo/gds/ldo.gds")
+
